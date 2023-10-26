7bbca38d53ecd4de8297105e6041eb3722d3ed13
Itmam Alam <115228081+itmam07@users.noreply.github.com>
2023-10-26 17:50:55 +0200

    add all

---
+++ bubblesort/bubblesort.h
@@ -0,0 +1,10 @@
+/*
+author: Itmam Alam
+file: bubblesort.cpp
+task: Arrays mit bubblesort sortieren
+date: 19.12.2022
+*/
+
+void fill(int a[], int len);
+void printArray(int a[], int len);
+void bubblesort(int a[], int len);
---
+++ quicksort.h
@@ -0,0 +1,2 @@
+void quicksort(int arr[], int left, int right);
+int getPivot(int arr[], int left, int right);
